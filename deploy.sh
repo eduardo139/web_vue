4808d4903dac4ae5a631c7a0842f595d8d04cd35
eduardo139 <A00826142@itesm.mx>
2022-03-27 13:13:25 -0600

    Corrected gh pages code

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env sh
-# abort on errors
 
+# abort on errors
 set -e
 
 # build
 npm run build
 
+# navigate into the build output directory
 cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m "Deploy"
+git commit -m 'deploy'
 
-git push-f git@github.com:eduardo139/web_vue.git master:gh-pages
+git push-f git@github.com:eduardo139/web_vue.git main:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports: {
-    publicPath: process.env.NODE_ENV === "production" ? "/web_vue/" : "/  "
+    publicPath: process.env.NODE_ENV === "production" ? "/web_vue/" : "/"
 }
