ccb0db5fd440118d30e86b39c8ac4691730c953e
eduardo139 <A00826142@itesm.mx>
2022-03-27 14:58:13 -0600

    tried to fix gh pages yet again...

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:eduardo139/web_vue.git master:gh-pages
+git push -f https://github.com/eduardo139/web_vue.git main:gh-pages
 
 cd -
