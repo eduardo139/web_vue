35f641f777df8250f426b5dd5606468addd44725
eduardo139 <A00826142@itesm.mx>
2022-03-27 13:24:29 -0600

    adding dist subtree

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push-f git@github.com:eduardo139/web_vue.git main:gh-pages
+git push-f git@github.com:eduardo139/web_vue.git master:gh-pages
 
 cd -
